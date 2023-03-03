b8c4342b107503fa23ab284a68181e54485ed47b
D4X1R3 <redaxire@gmail.com>
2023-03-03 21:43:08 +0100

    Add information about average balls and tasters

--- Summary_Functions_and_Maps.py
+++ Summary_Functions_and_Maps.py
@@ -3,5 +3,6 @@ pd.set_option('display.max_rows', 5)
 import numpy as np
 reviews = pd.read_csv("/users/arseniy/repos/datasets/winemag-data-130k-v2.csv", index_col=0)
 
-print('Information about tasters', reviews.taster_name.describe())
+
 print('The average wine has', round(reviews.points.mean()), 'points.')
+print('All unique tasters names are:', reviews.taster_name.unique())
