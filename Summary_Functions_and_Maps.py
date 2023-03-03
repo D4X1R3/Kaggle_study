61f7760175662422624b6ae7eb256a141d0dd0fb
D4X1R3 <redaxire@gmail.com>
2023-03-03 22:15:06 +0100

    Getting distribution pf points using the Map method

--- Summary_Functions_and_Maps.py
+++ Summary_Functions_and_Maps.py
@@ -4,5 +4,8 @@ import numpy as np
 reviews = pd.read_csv("/users/arseniy/repos/datasets/winemag-data-130k-v2.csv", index_col=0)
 
 
-print('The average wine has', round(reviews.points.mean()), 'points.')
-print('How often names occur in the dataset: \n', reviews.taster_name.value_counts())
+#calculating the mean of points using the 'mean()' metod of a pandas
+review_points_mean = reviews.points.mean()
+
+#using 'map' method to apply a lambda function 
+print(reviews.points.map(lambda p: p - review_points_mean))
