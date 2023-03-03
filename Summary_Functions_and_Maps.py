89dedd0e193fd63eaf2fe5c75d35ed3e46c83848
D4X1R3 <redaxire@gmail.com>
2023-03-03 22:37:59 +0100

    Getting distribution pf points using the Apply method and lefting only necessary columns

--- Summary_Functions_and_Maps.py
+++ Summary_Functions_and_Maps.py
@@ -1,11 +1,16 @@
 import pandas as pd
-pd.set_option('display.max_rows', 30)
+pd.set_option('display.max_rows', 5)
 import numpy as np
 reviews = pd.read_csv("/users/arseniy/repos/datasets/winemag-data-130k-v2.csv", index_col=0)
-
 
 #calculating the mean of points using the 'mean()' metod of a pandas
 review_points_mean = reviews.points.mean()
 
-#using 'map' method to apply a lambda function 
-print(reviews.points.map(lambda p: p - review_points_mean))
+#creating a function that takes a single argument row
+def remean_points(row):
+    row.points = row.points - review_points_mean
+    return row
+
+newform = reviews.apply(remean_points, axis='columns')
+
+print(newform.loc[:, ["country", 'points', 'title']])
