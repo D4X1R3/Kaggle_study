20c9dd32e60c4220d20fa7a36b14ba0cd6e31ec3
D4X1R3 <redaxire@gmail.com>
2023-03-03 21:13:47 +0100

    First commit

---
+++ pd_L.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+reviews=pd.read_csv("/users/arseniy/repos/DataSets/winemag-data-130k-v2.csv", index_col=0)
+pd.set_option('display.max_rows', 10)
+
+reviews['critic'] = 'everyone'
+pd.display(reviews['critic'])
