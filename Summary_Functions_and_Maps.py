54c996e6f5999f97bcad59c094249d2d4a879ae8
D4X1R3 <redaxire@gmail.com>
2023-03-03 21:59:25 +0100

    Add information about average wine and how often unique names occur in the dataset

--- Summary_Functions_and_Maps.py
+++ Summary_Functions_and_Maps.py
@@ -1,8 +1,8 @@
 import pandas as pd
-pd.set_option('display.max_rows', 5)
+pd.set_option('display.max_rows', 30)
 import numpy as np
 reviews = pd.read_csv("/users/arseniy/repos/datasets/winemag-data-130k-v2.csv", index_col=0)
 
 
 print('The average wine has', round(reviews.points.mean()), 'points.')
-print('All unique tasters names are:', reviews.taster_name.unique())
+print('How often names occur in the dataset: \n', reviews.taster_name.value_counts())
