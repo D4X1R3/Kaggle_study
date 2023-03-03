be0a3ae44330aba97efdfeb048bc1b95e1d8fca7
D4X1R3 <redaxire@gmail.com>
2023-03-03 22:44:58 +0100

    Faster way of getting points distribution

--- Summary_Functions_and_Maps.py
+++ Summary_Functions_and_Maps.py
@@ -6,11 +6,6 @@ reviews = pd.read_csv("/users/arseniy/repos/datasets/winemag-data-130k-v2.csv", 
 #calculating the mean of points using the 'mean()' metod of a pandas
 review_points_mean = reviews.points.mean()
 
-#creating a function that takes a single argument row
-def remean_points(row):
-    row.points = row.points - review_points_mean
-    return row
+#faster way to see points distribution
+print(reviews.points - review_points_mean)
 
-newform = reviews.apply(remean_points, axis='columns')
-
-print(newform.loc[:, ["country", 'points', 'title']])
